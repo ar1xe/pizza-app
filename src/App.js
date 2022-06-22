7288e1ac2b20fc1e97118f15553ba0974c94163b
ar1xe <chelseagrin52@gmail.com>
2022-06-22 23:19:36 +0300

    add skeleton and async request to the database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaCard from "./components/PizzaCard";
-import { pizzas } from "./constants";
+import { pizzaDB } from "./constants";
+import { PizzaCardSkeleton } from "./components/PizzaCardSkeleton";
 
 function App() {
+  const [items, setItem] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(pizzaDB);
+      const json = await response.json();
+      setItem(json);
+      setIsLoading(false);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
   return (
     <>
       <div className="wrapper">
@@ -19,17 +33,20 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((item) => (
-                <PizzaCard
-                  title={item.title}
-                  price={item.price}
-                  img={item.imageUrl}
-                  sizes={item.sizes}
-                  types={item.types}
-                  key={item.id}
-                />
-              ))}
-              {/* <PizzaCard title="Мексиканская" price={450} /> */}
+              {isLoading
+                ? [...new Array(15)].map((_, index) => (
+                    <PizzaCardSkeleton key={index} />
+                  ))
+                : items.map((item) => (
+                    <PizzaCard
+                      key={item.id}
+                      title={item.title}
+                      price={item.price}
+                      img={item.imageUrl}
+                      sizes={item.sizes}
+                      types={item.types}
+                    />
+                  ))}
             </div>
           </div>
         </div>
